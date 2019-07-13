8b4f0a0715ca319df43d3abc8e4d9b2b9f536a2a
jemaystermind <jemay.mastermind@gmail.com>
2019-07-13 18:39:00 +0800

    Ignore 'login' command if someone already logged in

--- src/main/java/dev/jemaystermind/dagger/tutorial/LoginCommand.java
+++ src/main/java/dev/jemaystermind/dagger/tutorial/LoginCommand.java
@@ -1,24 +1,33 @@
 package dev.jemaystermind.dagger.tutorial;
 
 import dev.jemaystermind.dagger.tutorial.Database.Account;
+import java.util.Optional;
 import javax.inject.Inject;
-
 
 final class LoginCommand extends SingleArgCommand {
   private final Outputter outputter;
   private final Database database;
   private final UserCommandsRouter.Factory userCommandRouterFactory;
+  private final Optional<Account> account;
 
   @Inject
-  LoginCommand(Database database, Outputter outputter,
-      UserCommandsRouter.Factory userCommandRouterFactory) {
+  LoginCommand(
+      Database database,
+      Outputter outputter,
+      UserCommandsRouter.Factory userCommandRouterFactory,
+      Optional<Account> account) {
     System.out.println("Creating a new " + this);
     this.outputter = outputter;
     this.database = database;
     this.userCommandRouterFactory = userCommandRouterFactory;
+    this.account = account;
   }
 
   @Override protected Result handleArg(String username) {
+    if (account.isPresent()) {
+      // Ignore "login <foo>" commands f we already have an account.
+      return Result.handled();
+    }
     Account account = database.getAccount(username);
     outputter.output(username + " is logged in with balance: " + account.balance());
     return Result.enterNestedCommandSet(userCommandRouterFactory.create(account).router());
--- src/main/java/dev/jemaystermind/dagger/tutorial/LoginCommandModule.java
+++ src/main/java/dev/jemaystermind/dagger/tutorial/LoginCommandModule.java
@@ -1,9 +1,12 @@
 package dev.jemaystermind.dagger.tutorial;
 
 import dagger.Binds;
+import dagger.BindsOptionalOf;
 import dagger.Module;
 import dagger.multibindings.IntoMap;
 import dagger.multibindings.StringKey;
+
+import static dev.jemaystermind.dagger.tutorial.Database.*;
 
 @Module
 abstract class LoginCommandModule {
@@ -12,4 +15,7 @@ abstract class LoginCommandModule {
   @IntoMap
   @StringKey("login")
   abstract Command loginCommand(LoginCommand command);
+
+  @BindsOptionalOf
+  abstract Account optionalAccount();
 }
